20fe6ab1e0d7089db5997789f0e13ac4aa58f69d
farfloat <near@far-float.de>
2022-02-27 23:49:19 +0900

    img domain

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
-module.exports = {};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+  images: {
+    domains: [process.env.NEXT_PUBLIC_DELIVERY_URL],
+  },
+};
+
+module.exports = nextConfig;
